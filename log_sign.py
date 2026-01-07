f7df8e60c326ad277e1e15e5fb594d7079834a10
Sonam097-eng <sonamvermalt97@gmail.com>
2026-01-07 16:48:28 +0530

    Added signup code

---
+++ log_sign.py
@@ -0,0 +1,23 @@
+from flask import Flask, request, session, Response
+import json
+
+app = Flask(__name__)
+app.secret_key = "mysecret"
+users = {}
+
+
+@app.route("/signup", methods =["POST"])
+def signup():
+    data = request.get_json()
+
+    username = data.get("username")
+    password = data.get("password")
+
+    if not username or not password:
+        return json_response({"Error":"Data not found"}, 400)
+    
+    if username in users:
+        return json_response({"error":"user already exist"}, 400)
+    
+    users[username] = username and users[password] = password
+    return json_response({"message":"signup succesful"}, 200)
