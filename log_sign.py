59bcbd5d564ce6809a59876ef11b0a683c54d5be
Sonam097-eng <sonamvermalt97@gmail.com>
2026-01-08 16:58:56 +0530

    added signup

--- log_sign.py
+++ log_sign.py
@@ -3,31 +3,57 @@ import json
 
 app = Flask(__name__)
 app.secret_key = "mysecret"
-users = {}
+
+
 
 
 @app.route("/signup", methods =["POST"])
 def signup():
-    data = request.get_json()
+    # To get data
+    resp= request.data
+    try:
+         resp_data= json.loads(resp.decode("utf-8"))
+    except Exception as e:
+        return({"result": "Fail","message":"Error has occured"}, 400)
+    #reference file
+    def read_file(path):
+        data=[]
+        with open(path, "r")as f:
+            data= f.read()
+            return json.loads(data)
+    def write_file(path,data):
+        if type(data)not in [dict,list]:
+            return{"result":"fail"}
+        with open(path, 'w')as f:
+            f.write(json.dumps(data))
+        return({"message":"Data saved"})    
 
-    username = data.get("username")
-    password = data.get("password")
 
-    if not username or not password:
-        return json_response({"Error":"Data not found"}, 400)
+    #checked missing data
+    missing_data=[]
+    if not resp_data.get("username", None):
+        missing_data.append("username")
+    if not resp_data.get("password", None):
+        missing_data.append("password")
+
+    if missing_data:
+         return({"result":"Fail","message":"Data not found"}, 400)
+    #checking if data matches
+    username= resp_data.get("username", None)
+    password= resp_data.get("password", None)
+
+    #checking if data is there or not
+    logged_data= read_file(path="database\login.json")
+    if logged_data.get("my_data").get("username", None):
+        return{"result":"fail","message":f"{username}username already exist"}
     
-    if username in users:
-        return json_response({"error":"user already exist"}, 400)
+    logged_data.get("my_data").update({username:password})
+    isSuccess, message= write_file(path="database\login.json",data=logged_data)
+
+    if isSuccess:
+        return{"result":"pass","message":f"{username} username update"}
+    return{"result":"fail","message":"updation failed"}
+
     
-    users[username] = username and users[password] = password
-    return json_response({"message":"signup succesful"}, 200)
-
-@app.route("/login", methods =["POST"])
-def login():
-    data = request.get_json()
-    username = data.get("username")
-    password = data.get("password")
-    if users["username"] = username and users["password"] = password:
-         return json_response({"message":"Login successfull"}, 200)
-    return json_response({"error":"Invalid Credentiald"}, 400)
-
+if __name__ == "__main__":
+    app.run(debug =True)
