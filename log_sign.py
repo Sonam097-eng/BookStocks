8235528acc12b57904770c823fa3e1b1984d53c5
Sonam097-eng <sonamvermalt97@gmail.com>
2026-01-13 21:52:12 +0530

    created new Api with authorization
    Signed-off-by: Sonam097-eng <sonamvermalt97@gmail.com>

--- log_sign.py
+++ log_sign.py
@@ -1,100 +1,195 @@
-from flask import Flask, request, session, Response,url_for,redirect
-import json
+from flask import Flask,request
+import json,random
 
-app = Flask(__name__)
+app=Flask(__name__)
 
-def get_all_books(file="database\\books.json"):
-    data={}
-    with open(file,'r')as f:
-        data=f.read()
-    return json.loads(data)
+def authorize(headers:dict):
+    users_token=headers.get("Authorization")
+    if not users_token:
+        return False, ({"result": "fail", "message": " user not authorized"}, 403)
+    token_value=read_file(path="database\\token.json")
+    if token_value.get("logged_data").get(users_token):
+        return True, ({"result":"Pass", "message": "User is Authorized"}, 200)
+    return False, ({"result":"fail", "message": "User not Authorized"}, 401)
 
+def generate_random():
+    return random.randint(1,100)   
 def read_file(path):
     data={}
     with open(path,'r')as f:
-        data= f.read()
+        data=f.read()
     return json.loads(data)
-    
+
 def write_file(path,data):
     if type(data) not in [dict,list]:
-        return ({"result":"fail","message":"data is not dict"}, 400)
-    with open(path,'w')as f:
+        return ({"result":"fail","message":"data not found"})
+    with open(path, 'w') as f:
         f.write(json.dumps(data))
-    return ({"data saved successfully"},200)    
+    return ({"Data saved successfully"},200)    
 
-@app.route("/books")
-def books():
-    return(get_all_books)
-
-@app.route("/signup", methods=["POST"])
-def signup():
-    resp= request.data
-    try:
-        resp_data=json.loads(resp.decode("utf-8"))
-    except Exception as e:
-        return ({"result":"fail","message":"data is not json"}, 400)
+@app.route("/books", methods = ["GET"])
+def get_books():
+    is_authorized , message = authorize(request.headers)
+    if not is_authorized:
+        return message
     
+    return (read_file(path = "database\\books.json").get("my_data"), 200)
+    
+
+@app.route("/books/<int:book_id>", methods = ["GET"])
+def books(book_id):
+    is_authorized , message = authorize(request.headers)
+    if not is_authorized:
+        return message
+
+    books = read_file(path = "database\\books.json")
+    for i in books.get("my_data"):
+        if i.get("id", -1) == book_id:
+            return (json.dumps(i), 200)
+    return ({"result": "fail", "message": f"No Book found for id: {book_id}"}, 400)
+
+@app.route("/add_book", methods = ["POST"])
+def add_book():
+    # authorize user
+    is_authorized , message = authorize(request.headers)
+    if not is_authorized:
+        return message
+    # take the data out from request named req
+    new = request.data
+    try:
+    # make it to json to check if it is json req_data
+        new_data = json.loads(new.decode("utf-8"))
+
+    except Exception as e:
+        return ({"result":"fail","message":"Data is not json"}, 404)
+
+    # Check if missing data in req_data e.g. title, author and cost is there.
+    missing_data = []
+    # If missing data there then return provide me missing data
+    if not new_data.get("title", None):
+        missing_data.append("title")
+    if not new_data.get("author", None):
+        missing_data.append("author")
+    if not new_data.get("cost", None):
+        missing_data.append("cost")
+
+    if missing_data:
+        return ({"result":"fail", "message":f"Data has {missing_data} missing elements"})  
+
+    # read the data from file books named books_data
+    book_data = read_file(path ="database\\books.json")
+    # prepare your data to be added (req_data) and then add new data to books_data eg. prepare your id field that should be len(books_data) + 1
+    new_data.update({"id": len(book_data)+ 1})
+
+    # write data to database books_data
+    book_data.append(new_data)
+
+    # if written successfully then return successfully then return req_data
+    is_updated, message = write_file(path ="database\\books.json", data = new_data)
+    if is_updated:
+        return (json.dumps(new_data), 201)
+    # if not able to write it to database then return 500 with a message
+    return ({"result":"fail", "message":"Something Went Wrong! Please try again!"}, 500)
+
+@app.route("/books/<int:book_id>" , methods = ["DELETE"])
+def delete_book(book_id):
+    is_authorized , message = authorize(request.headers)
+    if not is_authorized:
+        return message
+    # go through data of books
+    books = read_file(path="database\\books.json")
+    # delete book_id
+    new_books = []
+    for i in books:
+        if i["id"] != book_id:
+            new_books.append(i)
+    update = write_file(path="database\\books.json", data = new_books)  
+    #check whether book is there or not
+    if len(books) != len(new_books):
+    #if deleted update successfully deleted
+        return (json.dumps(book_id), 200)
+    #if not then error message
+    return ({"result":"fail","message":"book has not fond"},400)
+
+
+@app.route("/signup",methods=["POST"])
+def signup():
+    resp=request.data
+    try:
+        resp_data = json.loads(resp.decode("utf-8"))
+    except Exception as e:
+        return ({"result":"fail","message":"Data is not json"},400)
     missing_data=[]
-    if not resp_data.get("username", None):
+    if not resp_data.get("username",None):
         missing_data.append("username")
     if not resp_data.get("password",None):
         missing_data.append("password")
+
     if missing_data:
-        return ({"result":"fail","message":"missing data"}, 400)
-    
+        return ({"result":"fail","message": f"Required attributes missing: {missing_data}"},400)    
+
     username=resp_data.get("username")
     password=resp_data.get("password")
     if not username or not password:
-        return ({"result":"fail", "message":"Neither username nor password"}, 400)
+        return ({"result":"fail","message":"neither username nor password"},400)
 
     logged=read_file(path="database\\login.json")
-
-    if logged.get("my_data").get(username, None):
-        return ({"message":"user already exist"}, 400)
+    if logged.get("my_data").get(username,None):
+        return ({"result":"fail","message":"user already there"})
     
     logged.get("my_data").update({username:password})
-    if_its_success, message=write_file(path="database\\login.json",data=logged)
-    if if_its_success:
-        return ({"result":"pass"}, 200)
-    return ({"result":"fail"})
+    is_success, message=write_file(path="database\\login.json",data=logged)
+    if is_success:
+        return ({"result": "pass","message": "You are Logged In."}, 200)
+    return ({"result":"fail","message":"Something went wrong! Please try Again!"}, 500)
 
 @app.route("/login",methods=["POST"])
 def login():
-    resp= request.data
+    resp=request.data
     try:
-         resp_data=json.loads(resp.decode("utf-8"))
+        resp_data=json.loads(resp.decode("utf-8"))
     except Exception as e:
-        return({"result":"fail","message":"data is not json"})
-    username=resp_data.get("username")
-    password=resp_data.get("password")
+        return ({"result":"fail","message":"Data is not json"}, 400)
+    missing_data=[]
+    if not resp_data.get("username",None):
+        missing_data.append("username")
+    if not resp_data.get("password",None):
+        missing_data.append("password",None)
+    if missing_data:
+        return ({"result": "fail","message": f"Required attributes missing: {missing_data}"}, 403)
+    username=resp_data.get("username",None)
+    password=resp_data.get("password",None)
     if not username or not password:
-        return({"result":"fail","message":"Neither username nor password"})
-
+        return ({"result":"fail","message":"neither username nor password"}, 400)
+        
     logged=read_file(path="database\\login.json")
+    if logged.get("my_data").get(username) == password:
+        # generate token
+        token = generate_random()
 
-    if logged.get("my_data").get(username)==password:
-        return ({"result":"Pass","message":"you are logged in."})
-    return({"result":"fail","message":"Invalid Credentials"})
+        # read token file
+        token_value = read_file(path="database\\token.json")
 
-@app.route("/books_checkout_after_login", methods=["POST"])
-def books_checkout_after_login():
-    resp= request.data
-    try:
-         resp_data=json.loads(resp.decode("utf-8"))
-    except Exception as e:
-        return({"result":"fail","message":"data is not json"})
-    username=resp_data.get("username")
-    password=resp_data.get("password")
-    if not username or not password:
-        return({"result":"fail","message":"Neither username nor password"})
+        # update token in token_data
+        token_value.get("logged_data").update({token: "True"})
+        # write updated token_data to file
+        is_updated, message = write_file(path="database\\token.json", data=token_value)
+            # If success then return 200
+        if is_updated:
+            return ({"result": "pass","message": "You are Logged In.", "token": token }, 200)
+            # if failed then return 500
+        return ({"result":"fail","message":"Something went wrong"},500)    
 
-    logged=read_file(path="database\\login.json")
-    if logged.get("my_data").get(username)==password:
-        return redirect(url_for(books))
+if __name__ == "__main__":
+    app.run(debug=True)    
 
     
 
+        
 
 
-if __name__ == "__main__":
-    app.run(debug =True)
+
+
+
+
+        
--- restAPI2.py
+++ restAPI2.py
@@ -1,51 +1,75 @@
-from flask import Flask, jsonify, request
+from flask import Flask, request
 import json
 import copy
+import random
 
 app = Flask(__name__)
 
-# books = [
-#     {"id": 1, "title": "The Hobbit", "author": "J.R.R. Tolkien"},
-#     {"id": 2, "title": "1984", "author": "George Orwell"}
-# ]
-
 DEFAULT_FILE_PATH = "database\\books.json"
 
-def get_books_fromdb(file=DEFAULT_FILE_PATH):
-    books = {}
-    with open(file, "r") as f:
-        my_data = f.read()
-        books = json.loads(my_data)
-    return books
-
-def update_books_at_db(data, file=DEFAULT_FILE_PATH):
-    if(type(data) not in [dict, list]):
-        return False, "Data is not a dictionary"
-    with open(file, "w") as f:
+def read_file(file_path):
+    """This function read file present in file_path and return the json of that object"""
+    data = {}
+    with open(file_path, "r") as f:
+        data = f.read()
+    return json.loads(data)
+    
+def write_file(file_path, data):
+    if type(data) not in [dict, list]:
+        return False, "data provided was not JSON"
+    with open(file_path, "w") as f:
         f.write(json.dumps(data))
-    return True, "Data was written successfully"
+    return True, "Data save successfully"
 
-@app.route("/")
-def home():
-    return "<h1>HELLO WORLD</h1>"
+def authorize(header: dict):
+    # extract token
+    # print(type(header))
+    token_by_user = header.get("Authorization")
+    # Check if token was there. If not then return 403
+    if not token_by_user:
+        return False, ({"result": "Fail", "message": "No Authorization provided"}, 403)
+
+    # if Token is there then check if its is valid
+    token_data = read_file(file_path="database\\token.json")
+    value_in_token_data = token_data.get('logged_data').get(token_by_user)
+    if not value_in_token_data or str(value_in_token_data) in ["False"]:
+        return False, ({"result": "Fail", "message": "Please Log in First"}, 401)
+    return True, ("", 200)
+
+def generate_random():
+    return random.randint(20, 30)
 
 @app.route('/books', methods=['GET'])
 def get_books():
-    return (json.dumps(get_books_fromdb()), 200)
+    is_authorized, message_to_return = authorize(request.headers)
+    if not is_authorized:
+        return message_to_return
+    
+
+    return (read_file(DEFAULT_FILE_PATH), 200)
 
 
 @app.route('/books/<int:book_id>', methods=['GET'])
 def get_book(book_id):
-    books = get_books_fromdb()
+    is_authorized, message_to_return = authorize(request.headers)
+    if not is_authorized:
+        return message_to_return
+    
+    books = read_file(DEFAULT_FILE_PATH)
     for b in books:
         if b['id'] == book_id:
             return (json.dumps(b), 200)
     return {"result": "Pass", "message": f"Book not found"}, 404
     # book = next((b for b in books if b['id'] == book_id), None)
     # return json.dumps(book) if book else ("Book not found", 404)
-
+    
 @app.route('/books', methods=['POST'])
 def add_book():
+    is_authorized, message_to_return = authorize(request.headers)
+    if not is_authorized:
+        return message_to_return
+    
+
     new_book: dict = request.data
     try:
         new_book = json.loads(new_book.decode("utf-8"))
@@ -67,12 +91,12 @@ def add_book():
         return {"result": "Fail", "message": f"Mandatory Fields missing: {missing_data}"}, 400
     new_book_duplicate = copy.deepcopy(new_book)
     # if Data passed is successfully passing all checks then it is ready to fit into database
-    older_data = get_books_fromdb() 
+    older_data = read_file(DEFAULT_FILE_PATH)
      # Getting older data
     new_book.update({"id": len(older_data)+1})
     older_data.append(new_book)   # attaching new data to older one
     # print(older_data)
-    is_updated, message = update_books_at_db(data=older_data)  # writting new data to file/database
+    is_updated, message = write_file(file_path=DEFAULT_FILE_PATH, data=older_data)  # writting new data to file/database
     if is_updated:
         return json.dumps(new_book_duplicate), 200  # returning the data that I got as a sign that all check have passed and you data is successfully passed into file/databse
     else:
@@ -81,16 +105,101 @@ def add_book():
 
 @app.route('/books/<int:book_id>', methods=['DELETE'])
 def delete_book(book_id):
-    books = get_books_fromdb()
+    is_authorized, message_to_return = authorize(request.headers)
+    if not is_authorized:
+        return message_to_return
+
+    books = read_file(DEFAULT_FILE_PATH)
     new_books = []
     for b in books:
         if b["id"] != book_id:
             new_books.append(b)
-    update_books_at_db(new_books)
+    write_file(file_path=DEFAULT_FILE_PATH, data=new_books)
     if len(books) != len(new_books):
         return json.dumps({"message" : f"Book by id: {book_id} deleted successfully"}), 200
     else:
         return json.dumps({"message": "No book Found"}), 200
 
-if __name__ == '__main__':
+@app.route("/signup", methods =["POST"])
+def signup():
+    resp = request.data
+    try:
+        resp_data = json.loads(resp.decode("utf-8"))
+    except json.decoder.JSONDecodeError as e:
+        return ({"result": "Fail", "message": "Passed Data Format is not JSON"}, 404)
+    
+    missing_attribute = []
+    if not resp_data.get("username", None):
+        missing_attribute.append("username")
+    if not resp_data.get("password", None):
+        missing_attribute.append("password")
+    
+    if missing_attribute:
+        return ({"result": "Fail", "message": f"Required Attributes: {missing_attribute} were not present"}, 404)
+
+    username = resp_data.get("username")
+    password = resp_data.get("password")
+
+    # Check if username already exist return "Username already exist" to consumer
+    login_data = read_file(file_path="database\\login.json")
+    
+    if login_data.get("my_data").get(username, None):
+        return {"result": "Fail", "message": f"Username: {username} already there! Choose another"}, 200
+    
+    # If username is not there then update in database and return to consumer
+    login_data.get("my_data").update({username: password})
+    is_success, any_message = write_file(file_path="database\\login.json", data=login_data)
+    
+    if is_success:
+        return {"result": "Pass", "message": f"Username: {username}, successfully updated"}, 200
+    
+    return {"result": "Fail", "message": f"Username: {username}, updation Failed"}, 500
+
+    
+@app.route("/login", methods=["POST"])
+def login():
+    resp= request.data
+    try:
+         resp_data=json.loads(resp.decode("utf-8"))
+    except Exception as e:
+        return({"result":"fail","message":"data is not json"})
+    username=resp_data.get("username")
+    password=resp_data.get("password")
+    if not username or not password:
+        return({"result":"fail","message":"Neither username nor password"})
+
+    logged=read_file(file_path="database\\login.json")
+
+    if logged.get("my_data").get(username)==password:
+        token = generate_random()
+        token_data = read_file("database\\token.json")
+        token_data.get("logged_data").update({token: "True"})
+        is_written, message = write_file(file_path="database\\token.json", data=token_data)
+        if is_written:
+            return ({"result":"Pass","message":"you are logged in.", "token": token}, 200)
+        else:
+            return ({"result": "Fail", "message": "Server Error"}, 500)
+    return({"result":"fail","message":"Invalid Credentials"})
+
+@app.route("/logout", methods=["GET"])
+def logout():
+    is_authorized, message_to_return = authorize(request.headers)
+    if not is_authorized:
+        return message_to_return
+
+    token_data: dict = read_file(file_path="database\\token.json")
+    user_passed_token = request.headers.get("Authorization")
+    deleted_value = token_data.get("logged_data").pop(user_passed_token, None)
+    if not deleted_value:
+        return {"result" : "Fail", "message": "User not present"}, 404
+
+    is_written, message = write_file(file_path="database\\token.json", data=token_data)
+    if not is_written:
+        print(f"while deleting token, message: {message}")
+        return {"result": "Pass", "message": "Something Went Wrong!"}, 500
+    return {"result": "Pass", "message": "Successfully Loged Out!"}
+
+
+
+if __name__ == "__main__":
     app.run(debug=True)
